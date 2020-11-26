cf78998e3e4195eec580c9f076f5d8221bc5b0de
Bruna Uni Github <ricardob@coventry.ac.uk>
2020-11-26 16:07:00 +0000

    Logistic Regression and KNN Classifier completed. Done both times with feature scalling data and inicial dataset. Logistic Regression with Randomized Search and KNN Classifier with Grid Search. Also added cross validation

--- coursework/breast_cancer.py
+++ coursework/breast_cancer.py
@@ -6,10 +6,29 @@ import seaborn as sns
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import GridSearchCV, cross_val_score, train_test_split
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, cross_val_score, train_test_split, 
 
-#shows visualization in line -> replaces plt.show
-%matplotlib inline
+
+def logistic_regression(datatouse):
+    X = data.iloc[:,1:].values
+    Y = data.iloc[1:,0].values
+    #Scale X values to remove mean and improve accuracy
+    standard_scaler = StandardScaler()
+    X_scaled = standard_scaler.fit_transform(X)
+    #Defining training and testing variables
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled, Y, test_size=0.3, random_state=0)
+    #Training the model
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    y_train_pred = model.predict(X_train)
+    y_test_pred = model.predict(X_test)
+
+    #Printing results
+    print("Logistic Regression:\n")
+    print("Model training accuracy: ", model.score(X_train, y_train))
+    print("Model testing accuracy: ", model.score(X_test, y_test))
+    print("Model accuracy: ", model.score(y_test_pred, y_test))
